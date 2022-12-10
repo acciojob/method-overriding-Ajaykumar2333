5aad9190317990367054186eab36900644b714a7
ajaya <vodlaajay234@gmail.com>
2022-12-10 22:00:50 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static class A{
+
+        public  String Meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        public String Meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
+
+ public static  void main(String[] args){
+        B obj = new B();
+           obj.Meth();
+ }
 }
